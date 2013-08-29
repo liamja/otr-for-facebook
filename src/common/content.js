cc886b27b3c016bdec86f4af38b9237714144470
Liam Anderson <Liam.Anderson.91@gmail.com>
2013-08-29 15:40:41 +0100

    Require dependencies

--- src/common/content.js
+++ src/common/content.js
@@ -2,6 +2,12 @@
 // @name OTR for Facebook
 // @include http://*.facebook.com
 // @include https://*.facebook.com
+// @require lib/zepto.js
+// @require lib/dep/bigint.js
+// @require lib/dep/crypto.js
+// @require lib/dep/eventemitter.js
+// @require lib/dep/salsa20.js
+// @require lib/otr.js
 // ==/UserScript==
 
 console.log("OTR for Facebook loaded.");
