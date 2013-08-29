2db34e449f226e04af11f1dfecbc62a44a0b9724
Liam Anderson <Liam.Anderson.91@gmail.com>
2013-08-29 14:54:33 +0100

    Added grunt-contrib-clean grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,6 +71,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-shell');
 
   // Default task.
